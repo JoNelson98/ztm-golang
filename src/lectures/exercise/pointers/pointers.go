aed743e8701c76f7b60d00d9c2686d561a088724
Jonathan Nelson <jonathanator14@gmail.com>
2025-09-05 20:24:48 -0500

    pointers (demo + exercise)

--- src/lectures/exercise/pointers/pointers.go
+++ src/lectures/exercise/pointers/pointers.go
@@ -18,7 +18,42 @@ package main
 
 import "fmt"
 
-func main() {
+const (
+	Active   = true
+	Inactive = false
+)
 
+type SecurityTag bool
+
+type Item struct {
+	name string
+	tag  SecurityTag
 }
 
+var items = []Item{
+	{name: "item1", tag: true},
+	{name: "item2", tag: true},
+	{name: "item3", tag: true},
+	{name: "item4", tag: false},
+}
+
+func activate(tag *SecurityTag) {
+	*tag = Active
+}
+func deactivate(tag *SecurityTag) {
+	*tag = Inactive
+}
+
+func checkout(stuff []Item) {
+	fmt.Println("checking out...")
+	for i := range stuff {
+		deactivate(&stuff[i].tag)
+	}
+}
+
+func main() {
+	fmt.Println("initial items", items)
+	checkout(items)
+	activate(&items[0].tag)
+	fmt.Println("After items", items)
+}
